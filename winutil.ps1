1407340bc303fcc95dda8d1f7cafc84ab24fb341
zwpseudo <zackrey@mail.com>
2023-10-17 17:47:24 -0500

    Update winutil.ps1

--- winutil.ps1
+++ winutil.ps1
@@ -1203,25 +1203,15 @@ Function Invoke-WPFFormVariables {
 
 
     Write-Host ""
-    Write-Host "    CCCCCCCCCCCCCTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT   "
-    Write-Host " CCC::::::::::::CT:::::::::::::::::::::TT:::::::::::::::::::::T   "
-    Write-Host "CC:::::::::::::::CT:::::::::::::::::::::TT:::::::::::::::::::::T  "
-    Write-Host "C:::::CCCCCCCC::::CT:::::TT:::::::TT:::::TT:::::TT:::::::TT:::::T "
-    Write-Host "C:::::C       CCCCCCTTTTTT  T:::::T  TTTTTTTTTTTT  T:::::T  TTTTTT"
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C                     T:::::T                T:::::T        "
-    Write-Host "C:::::C       CCCCCC        T:::::T                T:::::T        "
-    Write-Host "C:::::CCCCCCCC::::C      TT:::::::TT            TT:::::::TT       "
-    Write-Host "CC:::::::::::::::C       T:::::::::T            T:::::::::T       "
-    Write-Host "CCC::::::::::::C         T:::::::::T            T:::::::::T       "
-    Write-Host "  CCCCCCCCCCCCC          TTTTTTTTTTT            TTTTTTTTTTT       "
+    Write-Host "       ▐███████ ,▄██`,▄██"
+    Write-Host "         ▄██▀ ,███ ,███-"
+    Write-Host "       ▄████▄████▄██▀"
+    Write-Host "      `▀▀▀▀▀▀▀▀▀▀▀▀"
     Write-Host ""
-    Write-Host "====Zackrey Watford Tech====="
-    Write-Host "=====Windows Toolbox====="
+    Write-Host ""
+    Write-Host ""
+    Write-Host "         Watford   "
+    Write-Host "     Windows Toolbox"
 
 
     #====DEBUG GUI Elements====
